8bdb65676f105e75afb9246276be2f59a1895a45
OhHyeonJu <71424881+OhHyeonJu0415@users.noreply.github.com>
2023-04-25 13:10:26 +0900

    Lv 2. 올바른 괄호

---
+++ 프로그래머스/올바른 괄호.cpp
@@ -0,0 +1,27 @@
+#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<int> S;
+
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='('){
+            S.push(1);
+        }else{
+            if(!S.empty()){
+                S.pop();
+            }else{
+                return false;
+            }
+        }
+    }
+    
+    if(!S.empty()) answer=false;
+
+    return answer;
+}

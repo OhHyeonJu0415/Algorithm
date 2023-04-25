7bd439ce872614c146ae5fd8bfd6963a798030bc
OhHyeonJu <71424881+OhHyeonJu0415@users.noreply.github.com>
2023-04-25 13:04:29 +0900

    Lv 2. 전화번호 목록

---
+++ 프로그래머스/전화번호 목록.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    string beforeText,nowText;
+    
+    sort(phone_book.begin(),phone_book.end());
+    
+    for(int i=0;i<phone_book.size()-1;i++){
+        beforeText=phone_book[i];
+        nowText=phone_book[i+1];
+        if(string::npos!=nowText.find(beforeText)){
+            for(int j=0;j<beforeText.size();j++){
+                if(beforeText[j]!=nowText[j]){
+                    break;
+                }
+                if(j==beforeText.size()-1)  return false;
+            }
+           
+        }
+        
+    }
+    return answer;
+}

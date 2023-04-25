d1f09033ce31cc3f7acba358f798f76d887004dc
OhHyeonJu <71424881+OhHyeonJu0415@users.noreply.github.com>
2023-04-25 13:08:50 +0900

    Lv 2. 영어 끝말잇기

---
+++ 프로그래머스/영어 끝말잇기.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(int n, vector<string> words) {
+    vector<int> answer(2,0);
+    map<string,int> m;
+    int Index=1;
+    m[words[0]]++;
+    int people=1;
+    int tern=1;
+    
+    while(Index<words.size()){
+        people++;
+        people=people%n;
+        m[words[Index]]++;
+        if(m[words[Index]]>=2 || words[Index-1].back()!=words[Index].front()){
+            if(people==0) people=n;
+            answer.front()=people;
+            answer.back()=tern;
+            break;
+        }
+        
+        Index++;
+        if(Index%n==0) tern++;
+    }
+
+    return answer;
+}

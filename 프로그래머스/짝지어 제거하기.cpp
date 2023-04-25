c7f06970bc633ad135e12cae0081e74b60678f5e
OhHyeonJu <71424881+OhHyeonJu0415@users.noreply.github.com>
2023-04-25 13:09:24 +0900

    Lv 2. 짝지어 제거하기

---
+++ 프로그래머스/짝지어 제거하기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    int answer = true;
+    stack<char> S;
+
+    for(int i=0;i<s.size();i++){
+        if(!S.empty()){
+            if(S.top()==s[i]) S.pop();
+            else S.push(s[i]);
+        }
+        else S.push(s[i]);
+    }
+    if(!S.empty()) answer=false;
+
+    return answer;
+}

fd11820fcba6f2eaa3ca362e8c4b0e5b41bc43c8
OhHyeonJu <71424881+OhHyeonJu0415@users.noreply.github.com>
2023-04-25 13:05:10 +0900

    Lv 2. 타겟 넘버

---
+++ 프로그래머스/타겟 넘버.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+int count=0;
+
+void dfs(int Index, vector<int> numbers,int target,int total){
+    if(Index>=numbers.size()){
+        if(total==target) count++;
+        return;
+    }
+    dfs(Index+1,numbers, target, total+numbers[Index]); 
+    dfs(Index+1,numbers, target, total-numbers[Index]);
+}
+
+int solution(vector<int> numbers, int target) {
+    count=0;
+    dfs(0,numbers,target,0);
+    return count;
+}
+

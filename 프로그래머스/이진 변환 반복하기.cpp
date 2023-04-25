1f4ea0781ea8937fd23f60c878928a9be3e50ad2
OhHyeonJu <71424881+OhHyeonJu0415@users.noreply.github.com>
2023-04-25 13:09:56 +0900

    Lv 2. 이진 변환 반복하기

---
+++ 프로그래머스/이진 변환 반복하기.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include<iostream>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer(2,0);
+    
+    while(s!="1"){
+        int len=s.size();
+        int zero=0;
+        
+        for(int i=0;i<len;i++){
+            if(s[i]=='0') zero++;
+        }
+        
+        answer.front()++;
+        answer.back()+=zero;
+        
+        //문자열 길이
+        len-=zero;
+        string v;
+        while(len>=1){
+            v+=to_string(len%2);
+            len=len/2;
+            if(len==1){
+                v+="1";
+                break;
+            }
+        }
+        reverse(v.begin(), v.end());
+        s=v;
+        
+    }
+    
+    return answer;
+}

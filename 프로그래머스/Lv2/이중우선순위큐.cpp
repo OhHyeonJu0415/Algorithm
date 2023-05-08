39a26b97d4c88102d326dac8ada55a492fe181df
OhHyeonJu <71424881+OhHyeonJu0415@users.noreply.github.com>
2023-05-08 16:19:47 +0900

    refactor : multiset 사용

--- 프로그래머스/Lv2/이중우선순위큐.cpp
+++ 프로그래머스/Lv2/이중우선순위큐.cpp
@@ -6,70 +6,29 @@ using namespace std;
 
 vector<int> solution(vector<string> operations) {
     vector<int> answer(2,0);
-    int countI=0;
-    int countD=0;
-    priority_queue <int, vector<int> > maxHeap;
-    priority_queue <int, vector<int>, greater<int> > minHeap;
-    map <int, int> m;
+    multiset<int> ms;
+    multiset<int>::iterator iter;
     
     for(int i=0;i<operations.size();i++){
         if(operations[i][0]=='I'){ //삽입
-            countI++;
-            int num = stoi(operations[i].substr(2,operations[i].size()));
-            maxHeap.push(num);
-            minHeap.push(num);
+            
+           ms.insert(stoi(operations[i].substr(2,operations[i].size())));
             
         }else if(operations[i][2]=='-'){ //최솟값 삭제
-            countD++;
-            if(countD==countI){
-                while(!minHeap.empty()) minHeap.pop();
-                while(!maxHeap.empty()) maxHeap.pop();
-                countD=0, countI=0;
-            }
-            if(!minHeap.empty()) minHeap.pop();
+           
+            if(!ms.empty()) ms.erase(ms.begin());
             
         }else{ //최댓값 삭제
-            countD++;
-            if(countD==countI){
-                while(!minHeap.empty()) minHeap.pop();
-                while(!maxHeap.empty()) maxHeap.pop();
-                countD=0, countI=0;
-            }
-            if(!maxHeap.empty()) maxHeap.pop();
+            
+            if(!ms.empty()) ms.erase(--ms.end());
         }
     }
     
-    while(!maxHeap.empty()){
-        m[maxHeap.top()]++;
-        maxHeap.pop();
-    }
-    while(!minHeap.empty()){
-        m[minHeap.top()]++;
-        minHeap.pop();
+    if(ms.size()!=0){
+        answer[0]=*(--ms.end());
+        answer[1]=*ms.begin();
     }
     
-    auto iter = m.begin();
-    int max=-99999999;
-    int min=99999999;
-    while(iter!=m.end()){
-        if(iter->second>=2){ //존재하는 값
-            int num = iter->first;
-            if(num<min) min=num;
-            if(num>max) max=num;
-        }
-        iter++;
-    }
-    if(min==99999999||max==-99999999){
-        return answer;
-    }
-    if(min==max){
-        if(min<0) answer[1]=min;
-        else answer[0]=min;
-    }
-    else{
-        answer[0]=max;
-        answer[1]=min;
-    }
     
     return answer;
 }

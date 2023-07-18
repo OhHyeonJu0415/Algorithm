88f3a9e1fdb67f367a13a522f36b56eaa2a34085
OhHyeonJu <71424881+OhHyeonJu0415@users.noreply.github.com>
2023-07-18 19:35:01 +0900

    Lv1. 부족한 금액 계산하기

---
+++ 프로그래머스/Lv1/부족한 금액 계산하기.py
@@ -0,0 +1,11 @@
+def solution(price, money, count):
+    answer = 0
+    temp=0
+    
+    for i in range(1,count+1):
+        temp+=price*i
+    
+    if(money<temp):
+        answer=temp-money
+
+    return answer
